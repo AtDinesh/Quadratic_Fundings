86d0ba147376651d0e1970de28f5d88372d40712
Dinesh Atchuthan <a.dinesh@hotmail.fr>
2024-05-31 00:11:22 +0200

    [lib fundin_round] add cqf allocation computation

--- funding_round/src/lib.rs
+++ funding_round/src/lib.rs
@@ -46,6 +46,36 @@ impl FundingRound {
             project.update();
         }
     }
+
+    // Capital Constrained Quadratic Funding
+    // Compute the CQF matching amount for the project.
+    // The initial allocated amount is computed independently for each project using QF method
+    // If the total allocation is > to matching pool M, then the initial allocation is 'normalized'
+    // through multiplying with M/(sum of all initial allocations)
+    pub fn compute_cqf_allocation(&mut self) -> HashMap<u32, f64> {
+        if self.matching_pool <= 0.0 {
+            panic!("Matching pool not set");
+        }
+        
+        let mut projects_final_funding = HashMap::new();
+        // initialize the map with qf-based fundings
+        for (id, project) in self.projects_list.iter() {
+            projects_final_funding.insert(*id, project.get_matching_amount());
+        }
+
+        let sum_matching_amount : f64 = self.projects_list.iter().map(|(_id, project)| project.get_matching_amount()).sum();
+        if sum_matching_amount <= self.matching_pool {
+            return projects_final_funding; // i.e. standard qf can be funded totally
+        }
+
+        // Otherwise, the pool is not enough to fund totally the projects
+        for (_,fund) in projects_final_funding.iter_mut() {
+            *fund = *fund * self.matching_pool.clone()/sum_matching_amount;
+        }
+
+        // projects_final_funding.iter_mut().map(|(key, value)| (key.clone(), *value * self.matching_pool.clone()/sum_matching_amount));
+        return projects_final_funding;
+    }
 }
 
 #[cfg(test)]
@@ -123,5 +153,51 @@ mod tests {
         let p_updated : Project = round.projects_list.get(&1).unwrap().clone();
         assert_eq!(contrib.amount, p_updated.get_total_contribution());
         assert_eq!(contrib.amount.clone().sqrt(), p_updated.get_sum_rootsquared_contribution());
+    }
+
+    #[test]
+    fn test_compute_cqf_allocation_keep_qf() {
+        // create the funding round
+        let mut round = FundingRound::new();
+        round.set_matching_pool(100.0);
+        // create projects for the round
+        let proj0 = Project::new(0);
+        let proj1 = Project::new(1);
+        round.add_project(proj0);
+        round.add_project(proj1);
+
+        let contrib0 = Contribution{from: 10, to: 0, amount: 5.0};
+        let contrib1 = Contribution{from: 10, to: 1, amount: 6.0};
+
+        round.add_contribution(contrib0);
+        round.add_contribution(contrib1);
+        round.update_projects();
+
+        // project0 has total contribution of 5 -> matching_fund is 5²=25
+        // project1 has total contribution of 6 -> matching fund is 6²=36
+        // sum of matching fund < matching pool.
+        // We shall keep the qf-based matching allocation.
+
+        let matching_alloc : HashMap<u32, f64> = round.compute_cqf_allocation();
+        assert_eq!(&round.projects_list.get(&0).unwrap().get_matching_amount(), matching_alloc.get(&0).unwrap());
+        assert_eq!(&round.projects_list.get(&1).unwrap().get_matching_amount(), matching_alloc.get(&1).unwrap());
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_compute_cqf_allocation_keep_qf_pool_unset() {
+        // create the funding round
+        let mut round = FundingRound::new();
+        
+        // create projects for the round
+        let proj0 = Project::new(0);
+        round.add_project(proj0);
+
+        let contrib0 = Contribution{from: 10, to: 0, amount: 5.0};
+
+        round.add_contribution(contrib0);
+        round.update_projects();
+
+        let _matching_alloc : HashMap<u32, f64> = round.compute_cqf_allocation();
     }
 }
